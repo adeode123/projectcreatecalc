8e8bbc30e91a0ff76bb6215679741136f5c11ad6
adeode123 <ao357@njit.edu>
2021-11-08 21:18:32 -0500

    Task:created new branch calc_part2

--- calculator/main.py
+++ calculator/main.py
@@ -1,4 +1,35 @@
 """ Calculator """
+class Calculator:
+
+    def __init__(self,first_num,second_num):
+        self.first_num = first_num
+        self.second_num = second_num
+        self.history = []
+
+    def get_history(self):
+        return self.history
+
+    def get_last_calculation(self):
+        return self.history[-1]
+
+    def get_first_calculation(self):
+        return self.history[0]
+
+    def add_calculation_to_history(self, record):
+        self.history.append(record)
+
+    def get_last_calculation_result(self):
+        return None
+
+    def last_calculation_object(self):
+        return None
+
+    def clear_history(self):
+        self.history = []
+
+    def count_history(self):
+        return len(self.history)
+
 
 def add(first_num, second_num):
     """ Test for addition """

89026abad185869103530ec520b830b27a4aabdc
adeode123 <ao357@njit.edu>
2021-11-09 21:59:34 -0500

    Task:Created improved calculator

--- calculator/main.py
+++ calculator/main.py
@@ -1,4 +1,6 @@
 """ Calculator """
+
+
 class Calculator:
 
     def __init__(self,first_num,second_num):
@@ -6,50 +8,150 @@ class Calculator:
         self.second_num = second_num
         self.history = []
 
-    def get_history(self):
-        return self.history
+    @staticmethod
+    def get_history():
+        """ History of calculator operations"""
+        return Calculator.calculator_history
 
-    def get_last_calculation(self):
-        return self.history[-1]
+    @staticmethod
+    def get_last_calculation():
+        """ Get last calculation"""
+        return Calculator.calculator_history[-1]
 
-    def get_first_calculation(self):
-        return self.history[0]
+    @staticmethod
+    def get_first_calculation():
+        """ Get first calculation"""
+        return Calculator.calculator_history[0]
 
-    def add_calculation_to_history(self, record):
-        self.history.append(record)
+    @staticmethod
+    def add_calculation_to_history(record):
+        """ Add calculation to calculation history """
+        Calculator.calculator_history.append(record)
+        return Calculator.calculator_history
 
-    def get_last_calculation_result(self):
-        return None
+    @staticmethod
+    def get_last_calculation_result():
+        """ Get last calculation result """
+        return Calculator.calculator_history[-1].get("result", None)
 
-    def last_calculation_object(self):
-        return None
+    @staticmethod
+    def last_calculation_object():
+        """ Get last calculation object """
+        return Calculator.calculator_history[-1].get("object", None)
 
-    def clear_history(self):
-        self.history = []
+    @staticmethod
+    def clear_history():
+        """ Clear history """
+        Calculator.calculator_history = []
+        return Calculator.calculator_history
 
-    def count_history(self):
-        return len(self.history)
+    @staticmethod
+    def count_history():
+        """ Get count of history items """
+        return len(Calculator.calculator_history)
+
+    @staticmethod
+    def addition(num_1, num_2):
+        """ Add """
+        return num_1 + num_2
+
+    @staticmethod
+    def subtraction(num_1, num_2):
+        """ Subtract """
+        return num_1 - num_2
+
+    @staticmethod
+    def multiplication(num_1, num_2):
+        """ Multiplication """
+        return num_1 * num_2
+
+    @staticmethod
+    def division(num_1, num_2):
+        """ Division """
+        try:
+            return num_1/num_2
+        except ZeroDivisionError:
+            print("Division by zero is not allowed")
+            return None
+
+    def factory(self, operation):
+        """ Factory helper method """
+        if operation == 'add':
+            add_object = Add(first_num=self.first_num, second_num=self.second_num)
+            result = add_object.add()
+            Calculator.add_calculation_to_history({
+                "result": result,
+                "object": add_object
+            })
+            return result
+
+        if operation == 'subtract':
+            subtract_object = Subtract(first_num=self.first_num, second_num=self.second_num)
+            result = subtract_object.subtract()
+            Calculator.add_calculation_to_history({
+                "result": result,
+                "object": subtract_object
+            })
+            return result
+
+        if operation == 'multiply':
+            multiply_object = Multiply(first_num=self.first_num, second_num=self.second_num)
+            result = multiply_object.multiply()
+            Calculator.add_calculation_to_history({
+                "result": result,
+                "object": multiply_object
+            })
+            return result
+
+        if operation == 'divide':
+            divide_object = Divide(first_num=self.first_num, second_num=self.second_num)
+            try:
+                result = divide_object.divide()
+                Calculator.add_calculation_to_history({
+                    "result": result,
+                    "object": divide_object
+                })
+                return result
+
+            except ZeroDivisionError:
+                print("Division by zero is not allowed")
+                return None
+
+        else:
+            return "Invalid Operation"
 
 
-def add(first_num, second_num):
-    """ Test for addition """
-    return first_num + second_num
+class Add(Calculator):
+    """ Add inherited from Calculator"""
+
+    def add(self):
+        """ Add from child class"""
+        return self.first_num + self.second_num
 
 
-def subtract(first_num, second_num):
-    """ Test for subtraction """
-    return first_num - second_num
+class Subtract(Calculator):
+    """ Subtract inherited from Calculator """
+
+    def subtract(self):
+        """ Subtract from child class"""
+        return self.first_num - self.second_num
 
 
-def multiply(first_num, second_num):
-    """ Test for multiplication"""
-    return first_num * second_num
+class Multiply(Calculator):
+    """ Multiply inherited from Calculator """
+
+    def multiply(self):
+        """ Multiply from child class """
+        return self.first_num * self.second_num
 
 
-def divide(first_num, second_num):
-    """Test for division"""
-    try:
-        return first_num/second_num
-    except ZeroDivisionError:
-        print("division by zero is not allowed")
-        return None
+class Divide(Calculator):
+    """ Divide inherited from Calculator """
+
+    def divide(self):
+        """ Divide from child class """
+        try:
+            return self.first_num / self.second_num
+        except ZeroDivisionError:
+            print("Division by zero is not allowed")
+            return None

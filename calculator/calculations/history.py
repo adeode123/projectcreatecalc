e7ccf493ec39a28f5b0978f6620557aef625a288
adeode123 <ao357@njit.edu>
2021-12-05 15:50:10 -0500

    updated main

--- calculator/calculations/history.py
+++ calculator/calculations/history.py
@@ -1,3 +1,6 @@
+"""created new module"""
+
+
 class History:
 
     """created History class"""
@@ -8,4 +11,30 @@ class History:
     def append_calculation(calculation):
         """ append function to add calculations"""
         return History.history.append(calculation)
+
+    @staticmethod
+    def clear_history():
+        """clears history"""
+        History.history.clear()
+        return True
+
+    @staticmethod
+    def count_history():
+        """returns number of items in the history"""
+        return len(History.history)
+
+    @staticmethod
+    def get_last_calculation():
+        """gets last calculation"""
+        return History.history[-1]
+
+    @staticmethod
+    def get_first_calculation():
+        """gets the first calculation"""
+        return History.history[0]
+
+    @staticmethod
+    def get_calculation(index):
+        """returns specific calculations from history"""
+        return History.history[index]
 

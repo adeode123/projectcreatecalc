0a07c7587eb13dbf9607c38e3ef7a81c4c9f7260
adeode123 <ao357@njit.edu>
2021-12-06 11:31:20 -0500

    created webCalc-part1 branch

--- calculator/calculations/addition.py
+++ calculator/calculations/addition.py
@@ -3,7 +3,9 @@ from calculator.calculations.calculation import Calculation
 
 class Addition(Calculation):
     """Created child class Addition"""
+
     def get_result(self):
+        """gets result"""
         result = 0.0
         for v in self.values:
             result = result + v
